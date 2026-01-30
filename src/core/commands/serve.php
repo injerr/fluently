926cc895677f0da896d7b7f03fc47194b5c1c29e
jeremy_intriago <jeremy.secundaria@gmail.com>
2026-01-31 00:15:21 +0100

    arreglado el serve --port

--- src/core/commands/serve.php
+++ src/core/commands/serve.php
@@ -3,7 +3,7 @@
 $port = "8000";
 
 if (isset($argv[1]) && str_contains($argv[1],"--port=")) {
-    if (preg_match('/^[0-9]{4}$/',$argv[1])) {
+    if (preg_match('/^[0-9]{4}$/',str_replace("--port=","",$argv[1]))) {
         $port = str_replace("--port=","",$argv[1]);
     }else{
         echo "El puerto indicado no es valido.\nUsando el puerto por defecto...\n";
